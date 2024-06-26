4d5c99014fd894c03faa8247840d01eb5afa22d1
RitsuMurakami <beinosuke01@gmail.com>
2024-06-26 18:06:40 +0900

    add: category selector

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,6 +11,16 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
+  List<String> categories = [
+    "Games",
+    "Tech",
+    "Tools",
+    "Paints",
+    "Musics",
+    "Users"
+  ];
+  int categoryIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,13 +34,44 @@ class HomeState extends State<Home> {
             child: Center(
                 child: Column(
           children: [
-            const SizedBox(height: 24),
+            const SizedBox(height: 64),
             Container(
                 margin: const EdgeInsets.all(8),
                 width: 512,
                 height: 257,
                 child: const Image(image: AssetImage("images/logo.png"))),
-            const SizedBox(height: 24),
+            const SizedBox(height: 64),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (int i = 0; i < categories.length; i++)
+                  Container(
+                      padding: const EdgeInsets.all(12),
+                      color: i == categoryIndex
+                          ? Theme.of(context).primaryColor
+                          : Theme.of(context).colorScheme.onPrimary,
+                      child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            categoryIndex = i;
+                          });
+                        },
+                        style: TextButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(0)),
+                        ),
+                        child: Text(
+                          categories[i],
+                          style: TextStyle(
+                              fontSize: 32,
+                              color: i == categoryIndex
+                                  ? Theme.of(context).colorScheme.onPrimary
+                                  : Theme.of(context).primaryColor),
+                        ),
+                      )),
+              ],
+            ),
+            const SizedBox(height: 16),
             SizedBox(
                 width: 728,
                 child: Divider(
