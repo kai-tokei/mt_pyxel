cf63e49db012e65bdea9cf97de4ef328a68dbc54
RitsuMurakami <beinosuke01@gmail.com>
2024-07-09 14:35:37 +0900

    add: comment

--- lib/pages/content_page.dart
+++ lib/pages/content_page.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 // components
-import 'package:mt_pyxel/components/common_appbar.dart';
-import 'package:mt_pyxel/components/common_bottombar.dart';
+import 'package:mt_pyxel/components/comment.dart';
 
 class ContentPage extends StatefulWidget {
-  const ContentPage({super.key});
+  const ContentPage({
+    super.key,
+    required this.title,
+    required this.author,
+    required this.image,
+    required this.likes,
+    required this.comments,
+    required this.desc,
+  });
+
+  final String title;
+  final String author;
+  final Widget image;
+  final int likes;
+  final int comments;
+  final String desc;
 
   @override
   State<ContentPage> createState() => ContentPageState();
@@ -14,119 +27,106 @@ class ContentPage extends StatefulWidget {
 class ContentPageState extends State<ContentPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CommonAppBar(
-        onAbout: () {},
-        onPlay: () {},
-        onLearn: () {},
-        onUserName: () {},
-      ),
-      body: SingleChildScrollView(
-          child: Center(
-              child: Column(
+    return Column(children: [
+      const SizedBox(height: 64),
+      Divider(thickness: 2.5, color: Theme.of(context).colorScheme.secondary),
+      Row(
         children: [
-          const SizedBox(height: 64),
-          Divider(
-              thickness: 2.5, color: Theme.of(context).colorScheme.secondary),
-          Row(
-            children: [
-              const SizedBox(width: 84),
-              Text("Simplest Application", style: TextStyle(fontSize: 48)),
-              const Expanded(child: SizedBox()),
-              Text("by Kitao",
-                  style: TextStyle(
-                      fontSize: 28,
-                      color: Theme.of(context).colorScheme.secondary)),
-              const SizedBox(width: 84),
-            ],
-          ),
-          Divider(
-              thickness: 2.5, color: Theme.of(context).colorScheme.secondary),
-          const SizedBox(height: 64),
-          Image.asset(
-            "images/gameview-sample.png",
-            width: 720,
-            fit: BoxFit.contain,
-          ),
-          SizedBox(
-            height: 84,
-            width: 700,
-            child: Row(
-              children: [
-                Image.asset("images/heart.png", width: 38),
-                const SizedBox(width: 4),
-                Text("136", style: const TextStyle(fontSize: 28)),
-                const SizedBox(width: 24),
-                Image.asset("images/comment.png", width: 38),
-                const SizedBox(width: 4),
-                Text("13", style: const TextStyle(fontSize: 28)),
-              ],
-            ),
-          ),
-          SizedBox(
-              width: 680,
-              child: Text(
-                """
-Welcome to "Hello, Pyxel!" - a simple yet charming introduction to the world of Pyxel. This project features an introductory screen that greets you with the text "Hello, Pyxel!" along with a Pyxel logo, showcasing the retro 8-bit aesthetic that Pyxel is known for. This project is perfect for beginners who want to get a feel for creating graphics and simple animations using Pyxel. Dive into the nostalgic world of pixel art and start your journey with Pyxel today!
-""",
-                style: TextStyle(fontSize: 24),
-              )),
-          const SizedBox(height: 16),
-          SizedBox(
-            width: 1000,
-            child: Column(
-              children: [
-                Divider(
-                    thickness: 2.5,
-                    color: Theme.of(context).colorScheme.secondary),
-                const Row(
-                  children: [
-                    SizedBox(width: 112),
-                    Text("Comments", style: TextStyle(fontSize: 48))
-                  ],
-                ),
-                Divider(
-                    thickness: 2.5,
-                    color: Theme.of(context).colorScheme.secondary)
-              ],
-            ),
-          ),
-          const SizedBox(height: 64),
-          SizedBox(
-              width: 830,
-              child: Column(
-                children: [
-                  TextField(
-                      style: const TextStyle(fontSize: 22),
-                      keyboardType: TextInputType.multiline,
-                      maxLines: 5,
-                      maxLength: 500,
-                      decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  width: 3,
-                                  color:
-                                      Theme.of(context).colorScheme.secondary),
-                              borderRadius: BorderRadius.circular(0)),
-                          enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  width: 3,
-                                  color:
-                                      Theme.of(context).colorScheme.secondary),
-                              borderRadius: BorderRadius.circular(0)))),
-                  Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                    TextButton(
-                      onPressed: () {},
-                      child:
-                          const Text("Comment", style: TextStyle(fontSize: 32)),
-                    ),
-                    const SizedBox(width: 32)
-                  ])
-                ],
-              )),
-          const CommonBottomBar()
+          const SizedBox(width: 84),
+          Text(widget.title, style: const TextStyle(fontSize: 48)),
+          const Expanded(child: SizedBox()),
+          Text(widget.author,
+              style: TextStyle(
+                  fontSize: 28,
+                  color: Theme.of(context).colorScheme.secondary)),
+          const SizedBox(width: 84),
         ],
-      ))),
-    );
+      ),
+      Divider(thickness: 2.5, color: Theme.of(context).colorScheme.secondary),
+      const SizedBox(height: 64),
+      widget.image,
+      //Image.asset(
+      //  "images/gameview-sample.png",
+      //  width: 650,
+      //  fit: BoxFit.contain,
+      //),
+      SizedBox(
+        height: 84,
+        width: 600,
+        child: Row(
+          children: [
+            Image.asset("images/heart.png", width: 38),
+            const SizedBox(width: 4),
+            Text(widget.likes.toString(), style: const TextStyle(fontSize: 28)),
+            const SizedBox(width: 24),
+            Image.asset("images/comment.png", width: 38),
+            const SizedBox(width: 4),
+            Text(widget.comments.toString(),
+                style: const TextStyle(fontSize: 28)),
+          ],
+        ),
+      ),
+      SizedBox(
+          width: 680,
+          child: Text(widget.desc, style: const TextStyle(fontSize: 24))),
+      const SizedBox(height: 32),
+      SizedBox(
+        width: 1000,
+        child: Column(
+          children: [
+            Divider(
+                thickness: 2.5, color: Theme.of(context).colorScheme.secondary),
+            const Row(
+              children: [
+                SizedBox(width: 112),
+                Text("Comments", style: TextStyle(fontSize: 48))
+              ],
+            ),
+            Divider(
+                thickness: 2.5, color: Theme.of(context).colorScheme.secondary)
+          ],
+        ),
+      ),
+      const SizedBox(height: 64),
+      SizedBox(
+          width: 630,
+          child: Column(
+            children: [
+              TextField(
+                  style: const TextStyle(fontSize: 22),
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 5,
+                  maxLength: 500,
+                  decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              width: 3,
+                              color: Theme.of(context).colorScheme.secondary),
+                          borderRadius: BorderRadius.circular(0)),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              width: 3,
+                              color: Theme.of(context).colorScheme.secondary),
+                          borderRadius: BorderRadius.circular(0)))),
+              Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(backgroundColor: Colors.white),
+                  child: Text("Comment",
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: Theme.of(context).colorScheme.onPrimary)),
+                )
+              ]),
+            ],
+          )),
+      const SizedBox(height: 32),
+      Comment(
+          image: Image.asset("images/gameview-sample.png",
+              width: 138, height: 138),
+          username: 'user1',
+          date: '2024/8/15',
+          comment: "This is the first comment."),
+    ]);
   }
 }
