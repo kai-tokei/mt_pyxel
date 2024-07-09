cb07a9009a2fd1d815a24473736c517e9628b6ba
RitsuMurakami <beinosuke01@gmail.com>
2024-07-09 13:04:37 +0900

    change: simplize

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 // components
-import 'package:mt_pyxel/components/common_appbar.dart';
-import 'package:mt_pyxel/components/common_bottombar.dart';
 import 'package:mt_pyxel/components/category_selector.dart';
 import 'package:mt_pyxel/components/common_searchbox.dart';
 import 'package:mt_pyxel/components/content.dart';
@@ -45,95 +43,84 @@ class HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: CommonAppBar(
-          onAbout: () {},
-          onPlay: () {},
-          onLearn: () {},
-          onUserName: () {},
-        ),
-        body: SingleChildScrollView(
-            child: Center(
-                child: Column(
-          children: [
-            const SizedBox(height: 64),
-            Container(
-                margin: const EdgeInsets.all(8),
-                width: 512,
-                height: 257,
-                child: const Image(image: AssetImage("images/logo.png"))),
-            const SizedBox(height: 64),
-            CategorySelector(
+    return Column(
+      children: [
+        const SizedBox(height: 64),
+        Container(
+            margin: const EdgeInsets.all(8),
+            width: 512,
+            height: 257,
+            child: const Image(image: AssetImage("images/logo.png"))),
+        const SizedBox(height: 64),
+        CategorySelector(
+            onPressed: (v) => setState(() {
+                  categoryIndex = v;
+                }),
+            categories: categories,
+            categoryIndex: categoryIndex),
+        const SizedBox(height: 16),
+        SizedBox(
+            width: 1000,
+            child: Divider(
+                thickness: 3, color: Theme.of(context).colorScheme.secondary)),
+        const SizedBox(height: 16),
+        SizedBox(
+            width: 950,
+            child: Row(children: [
+              CategorySelector(
                 onPressed: (v) => setState(() {
-                      categoryIndex = v;
-                    }),
-                categories: categories,
-                categoryIndex: categoryIndex),
-            const SizedBox(height: 16),
-            SizedBox(
-                width: 1000,
-                child: Divider(
-                    thickness: 3,
-                    color: Theme.of(context).colorScheme.secondary)),
-            const SizedBox(height: 16),
-            SizedBox(
-                width: 950,
-                child: Row(children: [
-                  CategorySelector(
-                    onPressed: (v) => setState(() {
-                      sortCategoryIndex = v;
-                    }),
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    categories: sortCategories,
-                    categoryIndex: sortCategoryIndex,
-                    fontSize: 24,
-                    categorySize: 4,
-                  ),
-                  const Expanded(child: SizedBox()),
-                  CommonSearchBox(onChanged: (v) {})
-                ])),
-            const SizedBox(height: 64),
-
-            // ここからコンテンツ
-            CategorySelector(
-                onPressed: (v) => setState(() {
-                      pageIndex = v;
-                    }),
-                categorySize: 0,
+                  sortCategoryIndex = v;
+                }),
+                mainAxisAlignment: MainAxisAlignment.start,
+                categories: sortCategories,
+                categoryIndex: sortCategoryIndex,
                 fontSize: 24,
-                categories: generatePageList(pageSize),
-                categoryIndex: pageIndex),
-            const SizedBox(height: 64),
-            // ここまでコンテンツ
+                categorySize: 4,
+              ),
+              const Expanded(child: SizedBox()),
+              CommonSearchBox(onChanged: (v) {})
+            ])),
+        const SizedBox(height: 64),
 
-            SizedBox(
-                width: 950,
-                child: Wrap(
-                  runSpacing: 16,
-                  spacing: 45,
-                  children: [
-                    for (int i = 0; i < 10; i++)
-                      ContentBox(
-                        image: Image.asset("images/gameview-sample.png",
-                            width: 281),
-                        title: "Jump game with Pyxel resource file",
-                        likes: 132,
-                        comments: 12,
-                        onPressed: () {},
-                      )
-                  ],
-                )),
-            const SizedBox(height: 64),
-            CategorySelector(
-                onPressed: (v) => setState(() {
-                      pageIndex = v;
-                    }),
-                categorySize: 0,
-                fontSize: 24,
-                categories: generatePageList(pageSize),
-                categoryIndex: pageIndex),
-            const CommonBottomBar()
-          ],
-        ))));
+        // ここからコンテンツ
+        CategorySelector(
+            onPressed: (v) => setState(() {
+                  pageIndex = v;
+                }),
+            categorySize: 0,
+            fontSize: 24,
+            categories: generatePageList(pageSize),
+            categoryIndex: pageIndex),
+        const SizedBox(height: 64),
+        // ここまでコンテンツ
+
+        SizedBox(
+            width: 950,
+            child: Wrap(
+              runSpacing: 16,
+              spacing: 45,
+              children: [
+                for (int i = 0; i < 10; i++)
+                  ContentBox(
+                    image:
+                        Image.asset("images/gameview-sample.png", width: 281),
+                    title: "Jump game with Pyxel resource file",
+                    likes: 132,
+                    comments: 12,
+                    onPressed: () {},
+                  )
+              ],
+            )),
+        const SizedBox(height: 64),
+        CategorySelector(
+            onPressed: (v) => setState(() {
+                  pageIndex = v;
+                }),
+            categorySize: 0,
+            fontSize: 24,
+            categories: generatePageList(pageSize),
+            categoryIndex: pageIndex),
+      ],
+    );
   }
 }
